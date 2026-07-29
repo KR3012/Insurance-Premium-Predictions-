f754c1b4d2a5e94dc8138e41b70b901ae6fbcf3d
Karan Rooprai <karanrooprai69@gmail.com>
2026-07-29 15:06:26 +0100

    finish all details plus annotation

--- analysis.py
+++ analysis.py
@@ -9,8 +9,9 @@ from sklearn.model_selection import train_test_split
 # this allows us to test the model on data it has never seen before
 from sklearn.metrics import r2_score, mean_squared_error
 # r2_score and mean_squared_error are functions that calculate how good the model is
-# r2_score = R² (how much of the pattern we captured)
-# mean_squared_error = calculating the RMSE (how wrong we are, in £)
+# r2_score = R² tells us the variation of the data thereforehow far our prediction is from the average 
+# having 0.617/ 61.7% tells us 61.7% of the variation in the data is explained by our model, the rest the model cannot explain for ex location 
+# root_mean_squared_error = RMSE (on average how far off in £ our guess is form average)
 
 data = pd.read_excel("InsurancePricingData_v1.xlsx")
 pd.set_option("display.max_columns",None)
@@ -20,14 +21,14 @@ X = data[["Age", "Years Driving", "Previous Claims", "Annual Mileage"]]
 Y = data["FinalPremium"]
 
 # spliting customers into 80% to train and 20% to test on
-# random_state=40 just makes the split the same every time we run it,
-# so our results are repeatable (40 is arbitrary — any number works)
+# random_state=40 just makes the split the same every time we run it, this allows our results to be repeated 
 X_train, X_test, Y_train, Y_test = train_test_split(
     X, Y, test_size=0.2, random_state=40
 )
 model = LinearRegression()
 model.fit(X_train, Y_train)
 predicted_premium = model.predict(X_test)
+# mkaing the model guess premiums only for the 20% test, therefore the ones it has never seen before
 
 r2 = r2_score(Y_test, predicted_premium)
 rmse = mean_squared_error(Y_test, predicted_premium) ** 0.5
@@ -38,7 +39,6 @@ plt.scatter(X_test["Age"], Y_test, label="Actual Premiums")
 # scatter means plotting dots as customers 
 # plt.plot(data["Age"], predicted_premium)
 plt.scatter(X_test["Age"], predicted_premium, label="Predicted Premiums")
-
 plt.xlabel("Age")
 plt.ylabel("Final Premium (£)")
 plt.title("Age/Final Premium")
